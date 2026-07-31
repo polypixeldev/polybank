71bc5daefac33d5b831dc5f70e4650fa8485323c
polypixeldev <me@sfernandez.dev>
2026-07-31 00:40:04 -0400

    Amount filters

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,13 +8,24 @@ class TransactionsController < ApplicationController
     @start_date = params[:start_date]
     @end_date = params[:end_date]
     @category = current_user.categories.find_by(id: params[:category_id])
+    @min_amount = params[:min_amount]
+    @max_amount = params[:max_amount]
+    @direction = params[:direction]
 
     @transactions = current_user.transactions.effective.order(pending: :desc, date: :desc)
 
-    @transactions = @transactions.where("memo LIKE ?", "%#{@query}%") if @query.present?
+    @transactions = @transactions.where("transactions.memo LIKE ?", "%#{@query}%") if @query.present?
     @transactions = @transactions.where("transactions.date >= ?", @start_date) if @start_date.present?
     @transactions = @transactions.where("transactions.date < ?", @end_date) if @end_date.present?
     @transactions = @transactions.where(category: @category) if @category.present?
+    @transactions = @transactions.where("abs(transactions.amount_cents) >= ?", @min_amount.to_f * 100) if @min_amount.present?
+    @transactions = @transactions.where("abs(transactions.amount_cents) < ?", @max_amount.to_f * 100) if @max_amount.present?
+
+    if @direction == "incoming"
+      @transactions = @transactions.where("transactions.amount_cents > 0")
+    elsif @direction == "outgoing"
+      @transactions = @transactions.where("transactions.amount_cents < 0")
+    end
 
     @total_amount = @transactions.sum(:amount_cents) / 100.0
   end
